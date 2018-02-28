70ba6e1d81538fd968c079d235017c8388bdd948
conas1290 <conas1290@7KZL6Y1.Student.UGDSB.ED>
2018-02-28 13:36:59 -0500

    This is my area of a rectangle project

---
+++ src/areaofrectangle/AreaofRectangle.java
@@ -0,0 +1,21 @@
+/*
+ * Corbin Nash
+ * AreaofRectangle.java
+ * This calculates 
+ */
+package areaofrectangle;
+/**
+ *
+ * @author conas1290
+ */
+public class AreaofRectangle {
+    public static void main(String[] args) {
+        double width = 5.7 ; //Sets up a variable for Width
+        double length = 4.8; //Sets up a variable for Length
+        double area = length * width;
+        System.out.println("The Width is "+width);
+        System.out.println("The Length is "+ length);
+        System.out.println("The Area is "+area);
+        
+    }
+}
